a3c0867fcfe170c4bcc0537300522e7dfd6b723e
makotolusi <lusimingwen@sina.com>
2016-04-20 11:47:31 +0800

    Initial Commit

---
+++ wodcn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wodcn
+//
+//  Created by 陆思 on 16/4/20.
+//  Copyright © 2016年 LUSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
